7d9a994d18b3b4d7023abb6714c895036a70bad3
Chef-Boyardeee <ericcartmanwizardking@gmail.com>
2025-08-03 20:36:34 +0800

    Progress 4

--- Assets/Scripts/GoalManager.cs
+++ Assets/Scripts/GoalManager.cs
@@ -6,6 +6,7 @@ public class GoalManager : MonoBehaviour
 {
     [SerializeField] private GoalItemInteractable[] goalItemInteractables;
     [SerializeField] private GoalPillar[] goalPillars;
+    [SerializeField] private Transform[] goalItemSpawns;
     [SerializeField] private MonsterAI monster;
     public static int maxScore;
     public static int currentScore;
@@ -17,7 +18,38 @@ public class GoalManager : MonoBehaviour
 
     private IEnumerator AwakeCoroutine()
     {
-        foreach(GoalPillar goalPillar in goalPillars)
+        //Check if there are enough goal item spawns
+        if(goalItemInteractables.Length > goalItemSpawns.Length)
+        {
+            Debug.LogError("Not enough spawn locations.");
+        }
+
+        //Reset used goal item spawns
+        Transform[] usedSpawns = new Transform[goalItemSpawns.Length];
+
+        //Initialize goal item spawns
+        foreach(GoalItemInteractable goalItem in goalItemInteractables)
+        {
+            bool goodSpawn = false;
+            int i = 0;
+            while (!goodSpawn)
+            {
+                i = Random.Range(0, goalItemSpawns.Length);
+                if(usedSpawns[i] == goalItemSpawns[i])
+                {
+                    goodSpawn = false;
+                }
+                else
+                {
+                    goalItem.transform.position = goalItemSpawns[i].position;
+                    usedSpawns[i] = goalItemSpawns[i];
+                    goodSpawn = true;
+                }
+            }
+        }
+
+        //Initialize goal pillars
+        foreach (GoalPillar goalPillar in goalPillars)
         {
             maxScore++;
             goalPillar.onAcceptItem += () =>
@@ -29,6 +61,7 @@ public class GoalManager : MonoBehaviour
                 if(currentScore >= maxScore)
                 {
                     //What happens when player places all goal items onto the pillars
+                    Debug.Log("Congratulations, you have escaped!");
                 }
             };
             goalPillar.onRejectItem += () =>
